b8630b69e2edc3b4099d2f3cbe89925b144fd1ce
Daniil <79607193+Im-Mad-Panda@users.noreply.github.com>
2021-05-22 21:55:44 +0300

    customers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-const swiper = new Swiper('.testimonials-slider', {
-  navigation: {
-    nextEl: '.slider-button-next',
-    prevEl: '.slider-button-prev',
+const testimonialsSlider = new Swiper('.testimonials-slider', {
+
+  breakpoints: {
+    768:{
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 20,
+    }
   },
-});
+
+  navigation: {
+    nextEl: '.arrow-left',
+    prevEl: '.arrow-right',
+  },
+  pagination: {
+    el: '.slider-pagination',
+    bulletClass: 'slider-bullet',
+    bulletActiveClass: 'slider-active-bullet'
+  },
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+})
